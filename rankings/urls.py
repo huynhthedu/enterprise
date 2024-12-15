77da4cd7072a5a03272ded41f8ea553c8fe1df47
huynhthedu <57565335+huynhthedu@users.noreply.github.com>
2024-12-14 23:37:57 -0500

    Update urls.py

--- rankings/urls.py
+++ rankings/urls.py
@@ -2,12 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.calculate_growth_and_rankings, name='rankings'),
-    path('', views.result, name='rankings'),
+    path('', views.calculate_growth_and_rankings, name='rankings'),
     path('indicator_map/', views.indicator_map, name='indicator_map'),
-    path('chart/', views.growth_chart, name='growth_chart'),
+    path('test/', views.growth_chart, name='growth_chart'),
     path('indicator_map/', views.shown_chart, name='shown_chart'),    
     path('scores/', views.score_and_rankings, name='score_and_rankings'),    
-    path('result/', views.result, name='result'),    
 ]
 
