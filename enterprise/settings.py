2689585d8a0331e3a0668ece1a44fe67d6ec3482
huynhthedu <duhtvn@gmail.com>
2024-12-15 10:01:16 -0500

    test

--- enterprise/settings.py
+++ enterprise/settings.py
@@ -99,11 +99,11 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'db',
-            'USER': db_info.username,
-            'PASSWORD': db_info.password,
-            'HOST': db_info.hostname,
-            'PORT': db_info.port,
+            'NAME': 'rankings',
+            'USER': 'postgres',
+            'PASSWORD': '123456',
+            'HOST': 'localhost',
+            'PORT': '5432',      
             'OPTIONS': {'sslmode': 'require'},
         }
     }
