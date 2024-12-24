2402c5d29983734b0d93da53ccf1fb8dd380576e
huynhthedu <duhtvn@gmail.com>
2024-12-23 21:53:29 -0500

    update

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rankings.views import calculations
 import json
+import requests
 
 def home(request):
     selected_indicators = 'Gross Domestic Product'  # Default indicator
@@ -13,9 +14,10 @@ def home(request):
 
     df1_dict = map_color.to_dict(orient='records')
 
-    # Load GeoJSON data with coordinates
-    with open('static/js/us-states.json') as f:
-        geojson_data = json.load(f)
+    # Fetch GeoJSON data from a URL
+    url = 'https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json'
+    response = requests.get(url)
+    geojson_data = response.json()
 
     # Merge ranking and score data with GeoJSON
     for feature in geojson_data['features']:
@@ -25,7 +27,7 @@ def home(request):
                 feature['properties']['rank'] = state['weighted_state_rank2']
                 feature['properties']['score'] = state['weighted_avg_score2']
                 break
-    # print(geojson_data)
+
     context = {
         'geojson_data': json.dumps(geojson_data),
         'selected_indicators': selected_indicators,
